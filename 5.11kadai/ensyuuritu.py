51908e7ada9edb1bdad4282ae5aaaee5eaefe77b
s19009 <s19009@std.it-college.ac.jp>
2020-05-25 09:35:15 +0900

    5/25 課題

---
+++ 5.11kadai/ensyuuritu.py
@@ -0,0 +1,12 @@
+moji = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+moji2 = moji.strip(".")
+moji3 = moji2.strip(",")
+risuto = moji3.split()
+
+numlist = []
+for lis in risuto:
+    num = len(lis)
+    numlist.append(num)
+
+print(numlist)
