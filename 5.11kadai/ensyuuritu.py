4779f0a50032c23043b561dc05f17b16278360f5
s19009 <s19009@std.it-college.ac.jp>
2020-05-25 09:53:01 +0900

    5/25 課題

--- 5.11kadai/ensyuuritu.py
+++ 5.11kadai/ensyuuritu.py
@@ -1,7 +1,7 @@
 moji = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 
 moji2 = moji.strip(".")
-moji3 = moji2.strip(",")
+moji3 = moji2.replace(",", "")
 risuto = moji3.split()
 
 numlist = []
