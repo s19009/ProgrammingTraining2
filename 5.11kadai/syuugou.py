7f64767c43938abb099b535ab8c2902f7c146523
s19009 <s19009@std.it-college.ac.jp>
2020-06-15 10:53:21 +0900

    5/15 課題

---
+++ 5.11kadai/syuugou.py
@@ -0,0 +1,18 @@
+def str_n_gram(text, n):
+    result = []
+    for i in range(len(text) - n + 1):
+        result.append(text[i : i + n : 1])
+    return set(result)
+
+
+sentence = "paraparaparadise"
+sentence2 = "paragraph"
+
+X = str_n_gram(sentence, 2)
+Y = str_n_gram(sentence2, 2)
+
+
+print(X | Y)
+print(X & Y)
+print(X - Y)
+print("se" in X.union(Y))
